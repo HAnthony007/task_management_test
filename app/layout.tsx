995ab274c190f8b58cb003c852151c4d5b7b687c
HAnthony007 <harifetraanthonyr@gmail.com>
2025-10-23 22:33:51 +0300

    feat: initial commit for task management app
    Sets up Next.js project with Tailwind CSS, ESLint, and
    basic components. Includes dashboard routes and UI setup.

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "sonner";
+import "./globals.css";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title: "Task Management",
+    description: "test recrutement dev front",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en" suppressHydrationWarning className="h-full">
+            <body
+                className={cn(
+                    geistSans.variable,
+                    geistMono.variable,
+                    "antialiased, h-full flex flex-col"
+                )}
+            >
+                <Toaster position="top-right" richColors />
+                <main className="flex-1">{children}</main>
+            </body>
+        </html>
+    );
+}
